5a76c0b14ec3dc6453bf6f8a9bf76f7db6a2ace0
JustinBenito <justinbenito1974@gmail.com>
2023-08-17 10:14:54 +0530

    ppt gesture controlled completed

--- ppt.py
+++ ppt.py
@@ -1,44 +1,3 @@
-# import cv2
-# import pyautogui
-# from cvzone.HandTrackingModule import HandDetector
-# import time
-# width, height = 1280, 720
-
-# cam = cv2.VideoCapture(1)
-# cam.set(3, width)
-# cam.set(4, height)
-# handdetector = HandDetector(detectionCon=0.8, maxHands=1)
-
-# while True:
-#     sucss, img = cam.read()
-#     hands, img = handdetector.findHands(img)
-#     if len(hands) > 0:
-#         hand = hands[0]
-#         fingers = handdetector.fingersUp(hand)
-#         print(fingers)
-#     if fingers == [0, 1, 0, 0, 0]:
-#         print("Option 1")
-#         #next slide
-#         pyautogui.press('right')
-#         time.sleep(2)
-#     if fingers == [0, 1, 1, 0, 0]:
-#         print("Option 2")
-#         #previous slide
-#         pyautogui.press('left')
-#         time.sleep(2)
-#     if fingers == [0, 1, 1, 1, 0]:
-#         print("Option 3")
-#         pyautogui.press('esc')
-#         time.sleep(2)
-#     if fingers == [0, 1, 1, 1, 1]:
-#         print("Option 3")
-#         pyautogui.press('q')
-#         time.sleep(2)
-#     cv2.imshow("Video", img)
-#     key = cv2.waitKey(1)
-#     if key == ord('q'):
-#         break
-
 import cv2
 import mediapipe as mp
 import pyautogui
@@ -80,20 +39,20 @@ while True:
 
         if fingers == [0, 1, 0, 0, 0]:
             print("Option 1")
-            #pyautogui.press('right')
-            # time.sleep(2)
+            pyautogui.press('right')
+            #time.sleep(0.1)
         elif fingers == [0, 1, 1, 0, 0]:
             print("Option 2")
-            #pyautogui.press('left')
-            #time.sleep(2)
+            pyautogui.press('left')
+            #time.sleep(0.1)
         elif fingers == [0, 1, 1, 1, 0]:
             print("Option 3")
-            #pyautogui.press('esc')
-            #time.sleep(2)
+            pyautogui.press('esc')
+            #time.sleep(0.1)
         elif fingers == [0, 1, 1, 1, 1]:
-            print("Option 3")
-            #pyautogui.press('q')
-            #time.sleep(2)
+            print("Option 4")
+            pyautogui.press('q')
+            #time.sleep(0.1)
 
     cv2.imshow("Video", img)
     key = cv2.waitKey(1)
